6d82af76c1d2b17030ebae9eb9958cf01572bcd0
AlbertLiuCST <albertliu1992@hotmail.com>
2020-04-03 14:45:20 -0700

    Fixed login, added login verification, fixed case-insensitivity for searches

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let router = express.Router();
 const home = require('../controllers/homepageController');
-const log = require('../controllers/loginController');
+const loginController = require('../controllers/loginController');
 const postController = require('../controllers/postController');
 const searchController = require('../controllers/searchController');
 
-router.post('/validateLogin',log.validateLogin);
+router.post('/validateLogin',loginController.validateLogin);
 
 router.post('/addPost',postController.addPost);
 
@@ -14,5 +14,9 @@ router.post('/search',searchController.searchByKeyword);
 router.post('/searchTopic',searchController.searchByTopic);
 
 router.get('/homepage',home.getHomePage);
+
+router.post('/addReply',postController.addReply);
+
+router.get('/logout',loginController.logout);
 
 module.exports = router;
