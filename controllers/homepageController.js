02257f1a6e1c3f49bc22da358bf270f1156d7e5c
AlbertLiuCST <albertliu1992@hotmail.com>
2020-04-04 18:15:34 -0700

    fixed replies not parsing properly on search page

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,8 +1,9 @@
 let modPost = require('../models/post');
 let modUser = require('../models/user');
+let parse = require('./parse')
 
-exports.getHomePage =  function(req,res,next, pageNumber = 0){
 
+exports.getHomePage = function(req,res,next, pageNumber = 0){
     if(req.cookies.signedIn !== "true"){
         res.redirect(301,'/');
         return
@@ -17,34 +18,13 @@ exports.getHomePage =  function(req,res,next, pageNumber = 0){
         
     let post = modPost.getRecentPostRe(req.cookies.pageNum);
     let prof = modUser.getUserByID(req.cookies.userid);
-    
+
     Promise.all([post,prof]).then((data)=>{
-        parsePosts(data[0].rows)        
+        parse.parsePosts(data[0].rows);
         res.render('homepage', {pageTitle:'Home Page',
         pageNum: req.cookies.pageNum,
         profile: data[1].rows[0],
         signedIn:true,
-        postList: data[0].rows,
-        postNotComplete: req.query.postNotComplete});
-    });
-}
-
-function parsePosts(rows){
-    let postList = rows;
-    postList.forEach(element => {
-        var replies = [];
-        if(element.r_text[0] != null){
-            for(let i = element.r_text.length - 1; i >= 0; i--){
-
-                let obj = {
-                        postid: element.postid,
-                        imgUrl : element.r_imgurl[i],
-                        replyText : element.r_text[i]
-
-                    }                
-                replies.push(obj);
-            }
-            element.replies = replies;
-        }
+        postList: data[0].rows});
     });
 }
