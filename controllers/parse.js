4d45fad508a39c32f261a79b8c1eae99576c1c45
AMiKaH <ameer.kubba@yahoo.com>
2020-04-07 02:21:08 -0700

    Edited post controller to provide the reply poster user id in the parse function, added a check for posts with no replies, made all images clickable and navigate to their user profile, minor css fixes to remove borders around images

--- controllers/parse.js
+++ controllers/parse.js
@@ -3,6 +3,8 @@ function parsePosts(rows,key = "",topic ="",userid=""){
     let postList = rows;
     postList.forEach(element => {
         var replies = [];
+        //console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        //console.log(element)
         if(element.r_text[0] != null){
 
             for(let i = element.r_text.length - 1; i >= 0; i--){
@@ -12,7 +14,8 @@ function parsePosts(rows,key = "",topic ="",userid=""){
                         posttopic: topic,
                         postuserid: userid,
                         imgUrl : element.r_imgurl[i],
-                        replyText : element.r_text[i]
+                        replyText : element.r_text[i],
+                        r_userid: element.r_userid[i]
 
                     }                
                 replies.push(obj);
